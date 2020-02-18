37fd86432baeec25211730a9b447bcc79ea6417c
Dmitry Pereverzev <vzorgv@gmail.com>
2020-02-18 11:38:57 +0300

    Graph creation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import asyncio
 import os
-from src import constants
 
+from graphviz import Digraph
+from src import constants
 from xml.etree.ElementTree import parse
+from pathlib import Path
+
+__package_graph = dict()
 
 
 def get_package_name(root, file_name: str):
@@ -25,10 +29,10 @@ def get_package_name(root, file_name: str):
     else:
         package = package_list[0]
 
-    return "Not a package" if not package else package
+    return "UNKNOWN" if not package else package
 
 
-async def parse_agent(file_name: str):
+async def parse_agent(file_name: str, dict_handle):
     tree = parse(file_name)
     root = tree.getroot()
 
@@ -38,13 +42,12 @@ async def parse_agent(file_name: str):
              for items in root if items.tag == constants.ITEMGROUP_TAG_NAME
              for reference in items if reference.tag == constants.PACKAGEREFERENCE_TAG_NAME]
 
-    #print(f"{root_package_name} - {file_name}")
-
+    packages_used = []
     for elem in items:
         package_name_used = elem.attrib.get(constants.REFERENCE_INCLUDE_ATTRIBUTE_NAME)
-        #print(package_name_used)
+        packages_used.append(package_name_used)
 
-    return root_package_name
+    dict_handle(root_package_name, packages_used)
 
 
 def get_files(root_dir: str):
@@ -54,23 +57,40 @@ def get_files(root_dir: str):
     return proj_files
 
 
+def append_package(package_name: str, packages_used: list):
+    list_exist = []
+
+    if package_name in __package_graph:
+        list_exist = __package_graph.get(package_name)
+
+    list_exist.extend(packages_used)
+    __package_graph[package_name] = list_exist
+
+
+def build_graph() -> object:
+    graph = Digraph('Components', node_attr={'color': 'lightblue2', 'style': 'filled'})
+
+    for vertex in __package_graph:
+        used_list = __package_graph[vertex]
+        for used in used_list:
+            if used is not None:
+                graph.edge(vertex, used)
+    home_dir = Path.home()
+    graph.save(filename='components.graph', directory=home_dir)
+    print(f"File saved in {home_dir}")
+
+
 async def main(root_dir: str):
     tasks = []
     files = get_files(root_dir)
-    '''
-    for file in files:
-        task = asyncio.create_task(parse_agent(file))
-        tasks.append(task)
-    
-    await asyncio.gather(*tasks, )
-    '''
 
     for file in files:
-        value = await parse_agent(file)
-        print(value)
-        #task = asyncio.create_task(parse_agent(file))
-        #tasks.append(task)
+        task = asyncio.create_task(parse_agent(file, append_package))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks, )
+    build_graph()
 
 
 if __name__ == "__main__":
-    asyncio.run(main("C:\\NetProjects"))
+    asyncio.run(main("C:\\Repo"))
