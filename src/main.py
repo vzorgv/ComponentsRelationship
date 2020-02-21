02f1d8282a625d0ee2ef2ce7ad10f2d2e61b85fc
Dmitry Pereverzev <vzorgv@gmail.com>
2020-02-21 18:12:40 +0300

    make async to sequence calls

--- src/main.py
+++ src/main.py
@@ -4,21 +4,14 @@ from src.agents.NugetFeedParser.ProgetFeedParser import ProgetFeedParser
 
 
 async def __main():
-    tasks = []
-
-
     feed_agent = ProgetFeedParser("http://proget.aeroclub.int/nuget/AeroclubRelease/Packages?$format=json")
-    task = asyncio.create_task(feed_agent.build_digraph())
-    tasks.append(task)
+    digraph = await feed_agent.build_digraph()
 
+    # await for nuget packages graph
     file_parser = VSProjectFileParser("C:\\Repo")
-    task = asyncio.create_task(file_parser.build_digraph())
-    tasks.append(task)
+    digraph_file = await file_parser.build_digraph()
 
-    await asyncio.gather(*tasks, )
-
-    digraph = tasks[0].result()
-    digraph.update(tasks[1].result())
+    digraph.update(digraph_file)
 
     return digraph
 
